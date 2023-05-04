7f3e5c5d3c50675b2a58ddce9dcdfe6bfea3e3b4
Abiya Baby <abiya@litmus7.com>
2023-05-04 11:47:57 +0530

    First commit

---
+++ file1.js
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+//file1 
+//this file is created in master branch
+//This is a line edited in master branch
+=======
+//hello
+//world
+//test
+//athother branch was created.
+//This is second line after creating the feature branch.
+>>>>>>> feature
